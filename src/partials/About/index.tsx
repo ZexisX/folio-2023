f704f6ee746f64d532e56440b08d525d2c7f6254
Lê Vĩnh Khang <127677696+levinhkhangzz@users.noreply.github.com>
2023-05-28 08:27:00 +0700

    Update index.tsx

--- src/partials/About/index.tsx
+++ src/partials/About/index.tsx
@@ -1,35 +1,24 @@
-import React from 'react';
-import style from './index.module.css';
+import style from './index.module.css'
 
 // Components
-import Section from 'components/Section';
-import Container, { Row } from 'components/Container';
-import ContentBlock from 'components/ContentBlock';
-import ImageTrigger from 'components/ImageTrigger';
-import Square from 'components/Square';
-import Heading from 'components/Heading';
-import { List, ListItem } from 'components/List';
+import Section from 'components/Section'
+import Container, { Row } from 'components/Container'
+import ContentBlock from 'components/ContentBlock'
+import ImageTrigger from 'components/ImageTrigger'
+import Square from 'components/Square'
+import Heading from 'components/Heading'
+import { List, ListItem } from 'components/List'
 
 // Hooks
-import { Trans, useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next'
 
 function About() {
-  const { t } = useTranslation('translation', { keyPrefix: 'about' });
-  const intro: string[] = t('intro', { returnObjects: true });
-  const clanTitle: string[] = t('clan_title', { returnObjects: true });
-  const clan: string[] = t('clan', { returnObjects: true });
-  const methodTitle: string = t('method_title');
-  const method: string[] = t('method', { returnObjects: true });
-
-  function increaseEndValue(startValue: number, count: number): string[] {
-    return Array.from({ length: count }, (_, index) => `x${startValue + index}`);
-  }
-
-  const awwwardsEndValues: string[] = increaseEndValue(6, 1029);
-  const cssDesignAwardsEndValues: string[] = increaseEndValue(6, 581);
-  const fwaEndValues: string[] = increaseEndValue(1, 751);
-  const ifDesignAwardEndValues: string[] = increaseEndValue(3, 980);
-  const otherEndValues: string[] = increaseEndValue(8, 2091);
+  const { t } = useTranslation('translation', { keyPrefix: 'about' })
+  const intro: string[] = t('intro', { returnObjects: true })
+  const clanTitle: string = t('clan_title')
+  const clan: string[] = t('clan', { returnObjects: true })
+  const methodTitle: string = t('method_title')
+  const method: string[] = t('method', { returnObjects: true })
 
   return (
     <Section name="about" className={style.root}>
@@ -43,7 +32,7 @@ function About() {
                   components={{
                     ImageVenice: <ImageTrigger name="venice" />,
                     ImageSketchin: <ImageTrigger name="sketchin" />,
-                    ImageTCMGTK: <ImageTrigger name="tcmgtk" sizes={[2, 3]} />,
+                    ImageTCMGTK: <ImageTrigger name="tcmgtk" sizes={[2, 3]} />
                   }}
                 />
               </div>
@@ -58,7 +47,7 @@ function About() {
               i18nKey="about.intro.1"
               components={{
                 Square: <Square />,
-                pre: <pre />,
+                pre: <pre />
               }}
             />
           </Heading>
@@ -74,7 +63,7 @@ function About() {
                   components={{
                     ImageVenice: <ImageTrigger name="venice" />,
                     ImageSketchin: <ImageTrigger name="sketchin" />,
-                    ImageTCMGTK: <ImageTrigger name="tcmgtk" sizes={[2, 3]} />,
+                    ImageTCMGTK: <ImageTrigger name="tcmgtk" sizes={[2, 3]} />
                   }}
                 />
               </div>
@@ -106,7 +95,7 @@ function About() {
                       ImageBW: <ImageTrigger name="bw" sizes={[2.5, 2.5]} />,
                       ImageNO1: <ImageTrigger name="no1" />,
                       ImageNO2: <ImageTrigger name="no2" />,
-                      ImageNO3: <ImageTrigger name="no3" sizes={[2, 2.5]} />,
+                      ImageNO3: <ImageTrigger name="no3" sizes={[2, 2.5]} />
                     }}
                   />
                 </div>
@@ -121,7 +110,7 @@ function About() {
                       ImageBW: <ImageTrigger name="bw" sizes={[2.5, 2.5]} />,
                       ImageNO1: <ImageTrigger name="no1" />,
                       ImageNO2: <ImageTrigger name="no2" />,
-                      ImageNO3: <ImageTrigger name="no3" sizes={[2, 2.5]} />,
+                      ImageNO3: <ImageTrigger name="no3" sizes={[2, 2.5]} />
                     }}
                   />
                 </div>
@@ -175,37 +164,16 @@ function About() {
         <Row start={1} end={3}>
           <ContentBlock>
             <List>
-              {awwwardsEndValues.map((endValue, index) => (
-                <ListItem key={index} end={endValue}>
-                  Awwwards
-                </ListItem>
-              ))}
-              {cssDesignAwardsEndValues.map((endValue, index) => (
-                <ListItem key={index} end={endValue}>
-                  CSS Design Awards
-                </ListItem>
-              ))}
-              {fwaEndValues.map((endValue, index) => (
-                <ListItem key={index} end={endValue}>
-                  FWA
-                </ListItem>
-              ))}
-              {ifDesignAwardEndValues.map((endValue, index) => (
-                <ListItem key={index} end={endValue}>
-                  iF Design Award
-                </ListItem>
-              ))}
-              {otherEndValues.map((endValue, index) => (
-                <ListItem key={index} end={endValue}>
-                  Other
-                </ListItem>
-              ))}
+              <ListItem end="x2092">Awwwards</ListItem>
+              <ListItem end="x1876">CSS Design Awards</ListItem>
+              <ListItem end="x511">FWA</ListItem>
+              <ListItem end="x713">iF Design Award</ListItem>
+              <ListItem end="x98">Other</ListItem>
             </List>
           </ContentBlock>
         </Row>
       </Container>
     </Section>
-  );
+  )
 }
-
-export default About;
+export default About
