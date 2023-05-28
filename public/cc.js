8d06e7c8a531d2731c6aab928b67e2a0f656fa92
Lê Vĩnh Khang <127677696+levinhkhangzz@users.noreply.github.com>
2023-05-28 11:24:10 +0700

    Update cc.js

--- public/cc.js
+++ public/cc.js
@@ -2,31 +2,31 @@ document.addEventListener('keydown', function(event) {
   if (event.ctrlKey && event.key === 's') {
     // Xử lý khi người dùng bấm Ctrl + S
     event.preventDefault(); // Ngăn chặn hành động mặc định của trình duyệt (ví dụ: lưu trang)
-    window.location.href = 'http://levinhkhang.xyz/404'; // Điều hướng đến URL khác
+    window.location.href = 'http://levinhkhang.xyz/'; // Điều hướng đến URL khác
   } else if (event.ctrlKey && event.key === 'u') {
     // Xử lý khi người dùng bấm Ctrl + U
     event.preventDefault(); // Ngăn chặn hành động mặc định của trình duyệt (ví dụ: hiển thị mã nguồn)
-    window.location.href = 'http://levinhkhang.xyz/404'; // Điều hướng đến URL khác
+    window.location.href = 'http://levinhkhang.xyz/'; // Điều hướng đến URL khác
   } else if (event.ctrlKey && event.key === 'i') {
     // Xử lý khi người dùng bấm Ctrl + I
     event.preventDefault(); // Ngăn chặn hành động mặc định của trình duyệt (ví dụ: mở công cụ kiểm tra)
-    window.location.href = 'http://levinhkhang.xyz/404'; // Điều hướng đến URL khác
+    window.location.href = 'http://levinhkhang.xyz/'; // Điều hướng đến URL khác
   } else if (event.key === 'F12') {
     // Xử lý khi người dùng bấm F12
     event.preventDefault(); // Ngăn chặn hành động mặc định của trình duyệt (ví dụ: mở công cụ phát triển)
-    window.location.href = 'http://levinhkhang.xyz/404'; // Điều hướng đến URL khác
+    window.location.href = 'http://levinhkhang.xyz/'; // Điều hướng đến URL khác
   }
 });
 
 document.addEventListener('contextmenu', function(event) {
   event.preventDefault(); // Ngăn chặn hiển thị menu ngữ cảnh mặc định
-  window.location.href = 'http://levinhkhang.xyz/404'; // Điều hướng đến URL khác
+  window.location.href = 'http://levinhkhang.xyz/'; // Điều hướng đến URL khác
 });
 
 document.addEventListener('DOMContentLoaded', function() {
   var currentUrl = window.location.href.toLowerCase();
   
   if (currentUrl === 'view-source:https://levinhkhang.xyz/' || currentUrl === 'view-source:https://levinhkhang.xyz') {
-    window.location.href = 'https://levinhkhang.xyz/404';
+    window.location.href = 'https://levinhkhang.xyz/';
   }
 });
