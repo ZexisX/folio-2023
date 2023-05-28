5209b1d47240243223394306f19143d5af90e345
Lê Vĩnh Khang <127677696+levinhkhangzz@users.noreply.github.com>
2023-05-28 11:27:04 +0700

    Update cc.js

--- cc.js
+++ cc.js
@@ -1,32 +1,37 @@
+var isRedirecting = false;
+
 document.addEventListener('keydown', function(event) {
-  if (event.ctrlKey && event.key === 's') {
-    // Xử lý khi người dùng bấm Ctrl + S
-    event.preventDefault(); // Ngăn chặn hành động mặc định của trình duyệt (ví dụ: lưu trang)
-    window.location.href = 'http://levinhkhang.xyz/404'; // Điều hướng đến URL khác
-  } else if (event.ctrlKey && event.key === 'u') {
-    // Xử lý khi người dùng bấm Ctrl + U
-    event.preventDefault(); // Ngăn chặn hành động mặc định của trình duyệt (ví dụ: hiển thị mã nguồn)
-    window.location.href = 'http://levinhkhang.xyz/404'; // Điều hướng đến URL khác
-  } else if (event.ctrlKey && event.key === 'i') {
-    // Xử lý khi người dùng bấm Ctrl + I
-    event.preventDefault(); // Ngăn chặn hành động mặc định của trình duyệt (ví dụ: mở công cụ kiểm tra)
-    window.location.href = 'http://levinhkhang.xyz/404'; // Điều hướng đến URL khác
-  } else if (event.key === 'F12') {
-    // Xử lý khi người dùng bấm F12
-    event.preventDefault(); // Ngăn chặn hành động mặc định của trình duyệt (ví dụ: mở công cụ phát triển)
-    window.location.href = 'http://levinhkhang.xyz/404'; // Điều hướng đến URL khác
+  if (event.ctrlKey && event.key === 's' && !isRedirecting) {
+    event.preventDefault();
+    redirect();
+  } else if (event.ctrlKey && event.key === 'u' && !isRedirecting) {
+    event.preventDefault();
+    redirect();
+  } else if (event.ctrlKey && event.key === 'i' && !isRedirecting) {
+    event.preventDefault();
+    redirect();
+  } else if (event.key === 'F12' && !isRedirecting) {
+    event.preventDefault();
+    redirect();
   }
 });
 
 document.addEventListener('contextmenu', function(event) {
-  event.preventDefault(); // Ngăn chặn hiển thị menu ngữ cảnh mặc định
-  window.location.href = 'http://levinhkhang.xyz/404'; // Điều hướng đến URL khác
+  event.preventDefault();
+  redirect();
 });
 
 document.addEventListener('DOMContentLoaded', function() {
   var currentUrl = window.location.href.toLowerCase();
   
-  if (currentUrl === 'view-source:https://levinhkhang.xyz/' || currentUrl === 'view-source:https://levinhkhang.xyz') {
-    window.location.href = 'https://levinhkhang.xyz/404';
+  if (currentUrl.startsWith('view-source:https://levinhkhang.xyz/404')) {
+    redirect();
   }
 });
+
+function redirect() {
+  if (!isRedirecting) {
+    isRedirecting = true;
+    window.location.href = 'http://levinhkhang.xyz/404';
+  }
+}
